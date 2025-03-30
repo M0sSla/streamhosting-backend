bd3e57e06da591ec5e99d401a880c0ed135809ef
Slava <mossla@inbox.ru>
2025-03-30 21:35:33 +0300

    account creation logic

--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -1,5 +1,7 @@
 import { PrismaService } from '@/src/core/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserInput } from './inputs/create-user.input';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AccountService {
@@ -12,5 +14,38 @@ export class AccountService {
         return users
     }
         
+    public async create(input: CreateUserInput) {
+        const {username, email, password} = input
 
+        const isUsernameExist = await this.prismaService.user.findUnique({
+            where: {
+                username
+            }
+        })
+
+        if (isUsernameExist) {
+            throw new ConflictException('Это имя пользователя уже занято')
+        }
+
+        const isEmailExist = await this.prismaService.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if (isEmailExist) {
+            throw new ConflictException('Эта почта уже занята')
+        }
+
+        const user = await this.prismaService.user.create({
+            data: {
+                username, 
+                email, 
+                password: await hash(password),
+                displayName: username
+            }
+        })
+
+        return true
+    }
 }
