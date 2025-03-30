732cc38a4a35ad6157a6b4ed304aa60888b435f3
Slava <mossla@inbox.ru>
2025-03-30 17:33:18 +0300

    created auth entity in graphql

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -6,19 +6,23 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { getGraphQLConfig } from './config/graphql.config';
 import { RedisModule } from './redis/redis.module';
+import { AccountModule } from '../modules/auth/account/account.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    ignoreEnvFile: !IS_DEV_ENV,
-    isGlobal: true
-  }),
-  GraphQLModule.forRootAsync({
-    driver: ApolloDriver,
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: getGraphQLConfig
-  }),
-  PrismaModule,
-  RedisModule]
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true
+    }),
+    GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getGraphQLConfig
+    }),
+    PrismaModule,
+    RedisModule,
+    AccountModule
+  ]
 })
 export class CoreModule {}
