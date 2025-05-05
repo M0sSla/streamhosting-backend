5dbf554722bddf01f561661ecf50bb22e3de7174
Slava <mossla@inbox.ru>
2025-05-06 00:36:57 +0300

    managing session auth

--- src/modules/auth/session/session.service.ts
+++ src/modules/auth/session/session.service.ts
@@ -1,20 +1,70 @@
 import { PrismaService } from '@/src/core/prisma/prisma.service';
-import { Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { LoginInput } from './inputs/login.input';
 import { verify } from 'argon2';
 import type { Request } from 'express'
 import { ConfigService } from '@nestjs/config';
+import { getSessionMetadata } from '@/src/shared/utils/session-metadata.util';
+import { RedisService } from '@/src/core/redis/redis.service';
+import session from 'express-session';
 
 @Injectable()
 export class SessionService {
 
     public constructor(
         private readonly prismaService: PrismaService,
+        private readonly redisService: RedisService,
         private readonly configService: ConfigService,
 
     ) {}
 
-    public async login(req: Request, input: LoginInput) {
+    public async findByUser(req: Request) {
+        const userId = req.session.userId
+
+        if (!userId) {
+            throw new NotFoundException('Пользователь не обнаружен в сессии')
+        }
+
+        const keys = await this.redisService.keys('*')
+
+        const userSessions: Array<Record<string, any>> = [];
+
+        for (const key of keys!) {
+            const sessionData = await this.redisService.get(key)
+
+            if (sessionData) {
+                const session = JSON.parse(sessionData)
+
+                if (session.userId === userId) {
+                    userSessions.push({
+                        ...session,
+                        id: key.split(':')[1]
+                    })
+                }
+            }
+        }
+
+        userSessions.sort((a, b) => b.createdAt - a.createdAt)
+
+        return userSessions.filter(session => session.id !== req.session.id)
+    }
+
+    public async findCurrent(req: Request) {
+        const sessionId = req.session.id
+
+        const sessionData = await this.redisService.get(
+            `${this.configService.getOrThrow<string>('SESSION_FOLDER')}${sessionId}`
+        )
+
+        const session = JSON.parse(sessionData as string)
+
+        return {
+            ...session,
+            id: sessionId
+        }
+    }
+
+    public async login(req: Request, input: LoginInput, userAgent: string) {
         const { login, password } = input
 
         const user = await this.prismaService.user.findFirst({
@@ -36,9 +86,12 @@ export class SessionService {
             throw new UnauthorizedException('Неверный пароль')
         }
 
+        const metadata = getSessionMetadata(req, userAgent)
+
         return new Promise((resolve, reject) => {
             req.session.createdAt = new Date()
             req.session.userId = user.id
+            req.session.metadata = metadata
 
             req.session.save(err => {
                 if (err) {
@@ -66,5 +119,26 @@ export class SessionService {
                 resolve(true)
             })
         })
+    }
+
+    public async clearSession(req: Request) {
+        req.res?.clearCookie(
+            this.configService.getOrThrow<string>('SESSION_NAME')
+        )
+
+        return true
+    }
+
+    public async remove(req: Request, id: string) {
+        
+        if (req.session.id === id) {
+            return new ConflictException('Вы не можете завершить свою сессию')
+        }
+
+        await this.redisService.del(
+            `${this.configService.getOrThrow<string>('SESSION_FOLDER')}${id}`
+        )
+
+        return true
     }
 }
