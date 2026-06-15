1027b8ebd079ff5ac2f246badf4c815efdca5411
Slava <mossla@inbox.ru>
2026-06-15 16:00:04 +0300

    some editing

--- src/main.ts
+++ src/main.ts
@@ -16,17 +16,6 @@ async function bootstrap() {
 
   const config = app.get(ConfigService)
   const redis = app.get(RedisService)
-  const allowedOrigins = config
-    .getOrThrow<string>('ALLOWED_ORIGIN')
-    .split(',')
-    .map(origin => origin.trim())
-    .filter(Boolean)
-
-  app.enableCors({
-    origin: allowedOrigins,
-    credentials: true,
-    exposedHeaders: ['set-cookie']
-  })
 
   app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
   app.use(config.getOrThrow<string>('GRAPHQL_PREFIX'), graphqlUploadExpress())
@@ -55,19 +44,18 @@ async function bootstrap() {
     name: config.getOrThrow<string>('SESSION_NAME'),
     resave: false,
     saveUninitialized: false,
-    // cookie: {
-    //   domain: config.getOrThrow<string>('SESSION_DOMAIN'),
-    //   maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
-    //   httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
-    //   secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
-    //   sameSite: 'lax'
-    // },
     cookie: cookieOptions,
     store: new RedisStore({
       client: redis,
       prefix: config.getOrThrow<string>('SESSION_FOLDER')
     })
   }))
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie']
+  })
 
   await app.listen(config.getOrThrow<number>('APPLICATION_PORT'))
 }
